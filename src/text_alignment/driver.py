d2e99eaf8fbc144be9b548cf3842801dd6416e71
MiralKumar1410 <miral.r@diggibyte.com>
2026-09-09 08:16:04 +0530

    Add text alignment driver

---
+++ src/text_alignment/driver.py
@@ -0,0 +1,6 @@
+from src.text_alignment.util import text_alingment
+
+if __name__ == '__main__':
+    thickness = int(input()) #This must be an odd number
+    c = 'H'
+    text_alingment(thickness,c) 
