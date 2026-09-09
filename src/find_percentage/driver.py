5426c89ab3e7c53a789a196b7a11e4a5c85cfae6
MiralKumar1410 <miral.r@diggibyte.com>
2026-09-09 08:15:08 +0530

    Add find percentage driver

---
+++ src/find_percentage/driver.py
@@ -0,0 +1,12 @@
+from util import average_score
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+        
+    print(average_score(student_marks,query_name))
