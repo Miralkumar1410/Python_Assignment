ec5fdc8697835fee7ad563a4e74e28807bd504a2
MiralKumar1410 <miral.r@diggibyte.com>
2026-09-09 11:06:23 +0530

    Add word order util

--- src/word_order/util.py
+++ src/word_order/util.py
@@ -1,11 +1,12 @@
 from collections import Counter 
 def word_order(n):
-    ls =[]
+    ls = []
+
     for i in range(n):
         row = str(input())
         ls.append(row)
+
     cntr = Counter(ls)
-    return (
-    print(len(cntr)),
+
+    print(len(cntr))
     print(*cntr.values())
-    )
